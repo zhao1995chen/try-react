5bee1092aa2419e711002ba0c78578321ae92bde
Zhao Chen <53460349+zhao1995chen@users.noreply.github.com>
2024-04-23 09:00:34 +0000

    feat: 🎸 create MyButton component

--- README.md
+++ README.md
@@ -1,8 +1,15 @@
-# React + Vite
+# React 筆記
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+- React component 須首字大寫命名
+  ```js
+  function MyButton() {
+    return (
+    )
+  }
+  ```
 
-Currently, two official plugins are available:
+- React `return` 只接受單一 tag，如果有多個 tag 要用 `<>` 在最外層包覆
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+## Tools
+
+- [HTML -> JSX 轉換器](https://transform.tools/html-to-jsx)
--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,23 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
+// React component 須首字大寫命名
+function MyButton() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <button onClick={() => setCount((count) => count + 1)}>
+        count is {count}
+      </button>
+    </>
+  )
+}
+
+function App() {
+  return (
+    <>
+      {/* React return 只接受單一 tag，如果有多個 tag 要用 <> 在最外層包覆 */}
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -18,9 +30,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <MyButton />
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
