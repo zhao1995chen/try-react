d042256a94404b5eef0e02531b43addd2a1c5041
Zhao Chen <53460349+zhao1995chen@users.noreply.github.com>
2024-04-24 10:46:12 +0000

    refactor: 💡 handleClick function

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,10 +3,12 @@ import { useState } from 'react'
 // React component 須首字大寫命名
 function MyButton() {
   const [count, setCount] = useState(0)
+  const handleClick = () => setCount((count) => count + 1)
 
   return (
     <>
-      <button onClick={() => setCount((count) => count + 1)}>
+      {/* Remind: 函數不需要帶括號 > 給定的是函數本身，不是執行函數 */}
+      <button onClick={handleClick}>
         count is {count}
       </button>
     </>
